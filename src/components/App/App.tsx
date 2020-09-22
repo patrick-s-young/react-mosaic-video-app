05918eac219140d84847e41a855c61148bc4153b
Patrick S. Young <patrick@bigeyeball.co>
2020-09-22 12:24:18 -0700

    Add <MosaicPreview> and <MosaicSelector> components to app
    Because
    - Unit testing
    
    <MosaicPreview> is a placeholder component that displays the
    state values it receives from redux. The full component will
    display only video so unit testing will be updated accordingly.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from 'Components/App/rootReducer';
+import MosaicPreview from 'Components/MosaicPreview/MosaicPreview';
+import MosaicSelector from 'Components/MosaicSelector/MosaicSelector';
 import 'Components/App/App.css';
 
 const App: React.FC = () => {
-	const { tileCount } = useSelector(
-		( state: RootState ) => state.project
-	);
 
 	return(
-			<div className='hello-flex'>
-				tileCount is {tileCount}
+			<div className='app'>
+				<MosaicPreview />
+				<MosaicSelector />
 			</div>
 	);
 }
