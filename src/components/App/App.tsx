bbc1b0d485c37bbd38651b043623aa2844bea354
Patrick S. Young <patrick@bigeyeball.co>
2020-09-23 13:20:46 -0700

    Add <BackgroundPreview> placeholder component as child of  <App>
    Because:
    - To allow unit testing
    - To aid visual layout
    
    <BackgroundPreview> will manage an array of <canvas>
    elements, each of which has a single frame of video
    drawn into it. The component receives a redux state
    value, 'backgroundFrame', that is an index into the
    array of <canvas> elements. At this point, the component
    has placeholder displays text that reflects the current value
    of 'backgroundFrame' for the purpuses of unit testing.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import MosaicPreview from 'Components/MosaicPreview/MosaicPreview';
 import MosaicSelector from 'Components/MosaicSelector/MosaicSelector';
+import BackgroundPreview from 'Components/BackgroundPreview/BackgroundPreview';
+import BackgroundSelector from 'Components/BackgroundSelector/BackgroundSelector';
 import FileSelector from 'Components/FileSelector/FileSelector';
 import 'Components/App/App.css';
 
@@ -10,6 +12,8 @@ const App: React.FC = () => {
 			<div className='app-container'>
 				<MosaicPreview />
 				<MosaicSelector />
+				<BackgroundPreview />
+				<BackgroundSelector />
 				<FileSelector />
 			</div>
 	);
