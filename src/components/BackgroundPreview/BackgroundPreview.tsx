12d141f4e8aa0683b92b3cb6b2b774cf43f13eec
Patrick S. Young <patrick@bigeyeball.co>
2020-09-25 14:38:53 -0700

    Add <BackgroundPreview> component and supporting utility
    Because:
    - Enables user to preview their <BackgroundSelector> designated  image
    - Utility gives developer asynch function to gather video metadata
    
    The <BackgroundPreview> component is passed a video file path
    along with the video's metadata (width, height, duration) and
    the number of frames to extract from the video (backgroundFramesMax).
    <CanvasVideoFrameCollection> creates an array of <CanvasVideoFrame>
    components which render a <canvas> that displays a 2d context-enabled
    frame of video. When the redux state value 'backgroundFrame' is updated
    by <BackgroundSelector>, the value is updated in <CanvasVideoFramesCollection>
    and the corresponding <CanvasVideoFrame> is shown.

--- src/components/BackgroundPreview/BackgroundPreview.tsx
+++ src/components/BackgroundPreview/BackgroundPreview.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'Components/App/rootReducer';
-import 'Components/BackgroundPreview/backgroundPreview.css';
+import CanvasVideoFrameCollection from 'Components/BackgroundPreview/CanvasVideoFrameCollection';
 
-const BackgroundPreview: React.FC = () => {
+interface BackgroundPreviewProps {
+  videoSrc: string
+  backgroundFramesMax: number
+  width: number
+  height: number
+  duration: number
+}
+
+const BackgroundPreview: React.FC<BackgroundPreviewProps> = ({
+  videoSrc, 
+  backgroundFramesMax,
+  width,
+  height,
+  duration }) => {
+
 	const { backgroundFrame } = useSelector(
 		( state: RootState ) => state.project
 	);
 
+	const currentTimes = useMemo<Array<number>>(() => { 
+    const timeIncrement: number = duration / backgroundFramesMax;
+    const currentTimesArr: Array<number> = [];
+    for (let i = 0; i < backgroundFramesMax; i++) {
+        currentTimesArr.push(i * timeIncrement + 0.1);
+    }
+    return currentTimesArr; 
+  }, [duration, backgroundFramesMax]);
+
 	return(
-			<div className='backgroundPreview-container' title='Background preview'>
+		<>
+			<div title='Background preview'>
 				[ backgroundFrame is {backgroundFrame}]
 			</div>
+			<div>
+				<CanvasVideoFrameCollection
+					currentTimes={currentTimes}
+					showFrameAtIndex={backgroundFrame}
+					videoSrc={videoSrc}
+					width={width}
+					height={height}
+				/>
+			</div>
+		</>
 	);
 }
 
