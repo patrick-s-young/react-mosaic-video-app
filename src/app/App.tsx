4ededb50c67636e55ca020bbbc3179aff84688bd
Patrick S. Young <patrick@bigeyeball.co>
2020-10-06 11:02:01 -0700

    refactored to incorporate <Scrubber> entry point

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,41 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import MosaicPreview from 'features/mosaicPreview/MosaicPreview';
 import MosaicSelector from 'features/mosaicPreview/MosaicSelector';
-import BackgroundPreview from 'features/backgroundPreview/BackgroundPreview';
-import BackgroundSelector from 'features/backgroundPreview/BackgroundSelector';
 import FileSelector from 'features/uploadDownload/FileSelector';
-import loadVideoMetadata, { VideoMetadata } from 'utils/loadVideoMetadata';
-import { backgroundFramesMax } from 'app/projectSlice';
+import { videoSrc } from 'app/app.config';
+import Scrubber from 'features/videoScrubber/Scrubber';
+
 import 'app/app.css';
 
-const App: React.FC = () => {
-  const [videoData, setVideoData] = useState<VideoMetadata | null>(null); 
-
-	const videoSrc = 'video/swing.mp4';
-
-  useEffect(() => {
-    loadVideoMetadata(videoSrc, 2000).then(data => setVideoData(data)).catch(error => console.log(error));
-	}, []);
-	
-	return(
+const App: React.FC = () =>
 			<div className='app-container'>
 				<MosaicPreview />
 				<MosaicSelector />
-				{ videoData !== null &&
-          <div style={{ width: videoData.videoWidth + 'px'}}>
-            <BackgroundPreview
-              videoSrc={videoSrc}
-              backgroundFramesMax={backgroundFramesMax}
-              width={videoData.videoWidth}
-              height={videoData.videoHeight}
-              duration={videoData.duration}
-            />
-          </div>
-        }
-				<BackgroundSelector />
+        <Scrubber
+          videoSrc={videoSrc}
+        />
 				<FileSelector />
 			</div>
-	);
-}
-
+      
 export default App;
